1241e072ded657679abf73eb54ada4cc14c96380
ankit_jamdagni <ankit.jamdagni@exsete.com>
2025-11-11 16:40:16 +0530

    vectorization complete

---
+++ file_structure.md
@@ -0,0 +1,15 @@
+```
+└── 📁GenAI
+    └── 📁dl_for_nlp
+    └── 📁ml_for_nlp
+        └── 📁Data
+            ├── spamhamdata.csv
+            ├── spamhamdata.pkl
+        ├── constraints.txt
+        ├── tokenization.py
+        ├── vectorization.py
+    ├── .gitignore
+    ├── main.py
+    ├── README.md
+    └── scrap.py
+```
--- ml_for_nlp/vectorization.py
+++ ml_for_nlp/vectorization.py
@@ -3,6 +3,7 @@ import pandas as pd
 import re
 
 
+
 def bow() -> None:
     """ spam/ham example(cleaning to vectorization) to vectorise the dataset.
     
@@ -42,7 +43,6 @@ def bow() -> None:
 
     def stemmer_and_stopwords(message:list) -> str:
         for index, word in enumerate(message):
-            
             if word not in set(stopwords.words('english')):
                 message[index] = stemmer.lemmatize(word=word, pos='v')
                 # print(f"{word} -----> {stemmer.lemmatize(word=word, pos='v')}")
@@ -69,19 +69,88 @@ def bow() -> None:
     
     # create bow
     from sklearn.feature_extraction.text import CountVectorizer
-    vectorizer = CountVectorizer(max_features=2500,binary=True)
-    x = vectorizer.fit_transform(df['cleaned_messages'])
+    bow_vectorizer = CountVectorizer(max_features=2500,binary=True)
+    x = bow_vectorizer.fit_transform(df['cleaned_messages'])
 
     print(x.shape)
+    print(bow_vectorizer.vocabulary_) # type: ignore
+
+def ngram() -> None:
+    # first run the BOW so that we bill be having pkl format data 
+    # cleaner function can be made global but for every technique cleaning procedure is same
+    # so use the existing pickle file
+
+    import os
+    pkl_file = r"ml_for_nlp/Data/spamhamdata.pkl"
+    if os.path.exists(pkl_file):
+        df = pd.read_pickle(pkl_file)
+
+    # create bow
+    from sklearn.feature_extraction.text import CountVectorizer
+
+    # check ngram (unigrams only(1,1), unigram and bifram(1,2), unigrma and trigram(1,3), 
+    # unigram and bigram(2,3), trigram only(3,3))
+    ngram_vectorizer = CountVectorizer(max_features=100,binary=True, ngram_range=(3,3))
+    x = ngram_vectorizer.fit_transform(df['cleaned_messages'])    # type: ignore
+
+    print(ngram_vectorizer.vocabulary_) # type: ignore
+
+def tfidf() -> None:
+    """ RERM FREQUENCY INVERSE DOCUMNET FREQUENCY """
+    # first run the BOW so that we bill be having pkl format data 
+    # cleaner function can be made global but for every technique cleaning procedure is same
+    # so use the existing pickle file
+
+    import os
+    pkl_file = r"ml_for_nlp/Data/spamhamdata.pkl"
+    if os.path.exists(pkl_file):
+        df = pd.read_pickle(pkl_file)
+
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    
+    tfidf_vectorizer = TfidfVectorizer(max_features=100, ngram_range=(2,2))
+    x = tfidf_vectorizer.fit_transform(df["cleaned_messages"]) # type: ignore
+    # print(x)
+
+    for word,index in tfidf_vectorizer.vocabulary_.items():
+        print(word, index)
+
+def word2vec() -> None:
+    import gensim
+
+    from gensim import downloader as api
+    from gensim.models import word2vec, keyedvectors
 
 
 
+    w2v_model = api.load("word2vec-google-news-300")
 
+    # model = Word2Vec.load("my_model.model")
+    print("Model loaded successfully!")
+    # model_path = r"ml_for_nlp/Data/word2vec-google-news-300.model"
+    # model = keyedvectors.load_word2vec_format(model_path, binary=True)
+    
+    king_vector = w2v_model['king'] # type: ignore
+    queen_vector = w2v_model['queen'] # type: ignore
+    
+    print("Vector for 'king':")
+    print(king_vector[:10])
 
+    print("\nVector for 'queen':")
+    print(queen_vector[:10])
 
+    print(w2v_model.most_similar('king')) # type: ignore
+
+    # # Famous analogy,also mentioned in google papers: king - man + woman = queen
+    result = w2v_model.most_similar(positive=['king', 'woman'], negative=['man']) # type: ignore
+    print(result)
 
 def main() -> None:
-    bow()
+    # bow()
+    # ngram()
+    # tfidf()
+    word2vec()
+    ...
 
 if __name__ == "__main__":
     main()
